3055c9e2569759b3da4852e4ab11f235c26edab5
Abdullah Shahbaz <abdullah_ghani@live.com>
2023-10-23 23:33:09 +0500

    added configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  env: {
+    node: true,
+    browser: true,
+  },
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["../../tsconfig.eslint.json"],
+  },
+  plugins: ["@typescript-eslint", "simple-import-sort"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  rules: {
+    curly: "error",
+    "@typescript-eslint/no-non-null-assertion": "warn",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        prefer: "type-imports",
+        disallowTypeAnnotations: true,
+        fixStyle: "separate-type-imports",
+      },
+    ],
+    "simple-import-sort/imports": "error"
+  },
+};

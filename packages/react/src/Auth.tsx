a4ec7bcfbc3c9056f607750c3efa6a476d25ed46
Isah Ohieku <isahohieku@gmail.com>
2023-10-27 09:45:38 +0100

    Fix root export and package usage

--- packages/react/src/Auth.tsx
+++ packages/react/src/Auth.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import Signup from './pages/Signup';
 import './styles.css';
 
-const Auth = () => {
+export const Auth = () => {
     return (
         <Signup />
     );
 }
-
-export default Auth;
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,1 @@
-export { default as CorbadoAuth } from './Auth';
+export * from './Auth';

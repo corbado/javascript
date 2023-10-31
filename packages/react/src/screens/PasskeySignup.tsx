048a0e2d8bbf3eea2cc3fce9e888141d5722937d
Isah Ohieku <isahohieku@gmail.com>
2023-10-31 22:25:51 +0100

    Setup passkey signup screen

--- packages/react/src/screens/PasskeySignup.tsx
+++ packages/react/src/screens/PasskeySignup.tsx
@@ -1,33 +1,53 @@
 import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import Text from '../components/Text';
-import Button from '../components/Button';
-import { HorizontalRule } from '../components/HorizontalRule';
+import { ButtonType, PasscodeScreensWrapper } from '../components/PasscodeScreensWrapper';
 
 export const PasskeySignup = () => {
     const { t } = useTranslation();
 
     const email_address = 'janina+test125@corbado.com';
 
+    const header = <Trans i18nKey="passkey_signup.header">
+        text <span className="text-primary-color underline">x</span> text
+    </Trans>;
+
+    const subHeader = <Trans i18nKey="passkey_signup.sub-header">
+        text <span className="text-secondary-font-color">{{ email_address } as any}</span> text
+    </Trans>;
+
+    const primaryButton = t('passkey_signup.primary_btn');
+    const secondaryButton = t('passkey_signup.secondary_btn');
+    const tertiaryButton = t('passkey_signup.tertiary_btn');
+
+    const handleCreateAccount = () => console.log('Create Account with Passkey');
+    const handleSendOtp = () => console.log('Send email OTP');
+    const handleBack = () => console.log('Go Back');
+
+    const handleClick = (btn: ButtonType) => {
+
+        if (btn === 'primary') {
+            return handleCreateAccount();
+        }
+        if (btn === 'secondary') {
+            return handleSendOtp();
+        }
+        handleBack();
+    }
+
+    const props = {
+        header,
+        subHeader,
+        primaryButton,
+        secondaryButton,
+        tertiaryButton,
+        onClick: handleClick
+    }
+
     return (
         <>
-            <Text variant="header">
-                {/* "text" is a placeholder value for translations */}
-                <Trans i18nKey="passkey_signup.header">
-                    text <span className="text-primary-color underline">x</span> text
-                </Trans>
-            </Text>
-            <Text variant="sub-header" className='mt-4'>
-                {/* "text" is a placeholder value for translations */}
-                <Trans i18nKey="passkey_signup.sub-header">
-                    text <span className="text-secondary-font-color">{{email_address} as any}</span> text
-                </Trans>
-            </Text>
-            <div className="finger-print-icon mx-auto"></div>
-            <Button variant='primary'>{t('passkey_signup.primary_btn')}</Button>
-            <HorizontalRule />
-            <Button variant='secondary'>{t('passkey_signup.secondary_btn')}</Button>
-            <Button className='my-0 !py-0' variant='tertiary'>{t('passkey_signup.tertiary_btn')}</Button>
+            <PasscodeScreensWrapper
+                {...props}
+            />
         </>
     )
 }

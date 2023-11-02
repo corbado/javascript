afc0fad16983af9241c8a2ac441f968490a2dfe7
Isah Ohieku <isahohieku@gmail.com>
2023-11-02 19:48:58 +0100

    Refactor Playground

--- playground/react-custom-ui/src/App.jsx
+++ playground/react-custom-ui/src/App.jsx
@@ -1,10 +1,10 @@
 import { ApiTest } from "./components/apiTest";
-// import { CorbadoProvider } from "@corbado/react-sdk";
+import { CorbadoProvider } from "@corbado/react-sdk";
 
 export function App() {
-  return (
+  return <CorbadoProvider projectId="pro-503401103218055321">
     <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: 100 }}>
       <ApiTest />
-    </div>)
-  // return <CorbadoProvider projectId="pro-503401103218055321"><ApiTest /></CorbadoProvider>
+    </div>
+  </CorbadoProvider>
 }

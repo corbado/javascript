3c8e03f4e5197bed6b376cbe82121bc59febb0b7
Abdullah Shahbaz <abdullah_ghani@live.com>
2023-11-03 13:37:30 +0500

    added flow types

--- packages/web-core/src/types/common.ts
+++ packages/web-core/src/types/common.ts
@@ -1,4 +1,10 @@
-import type { AssetsApi, ProjectsApi, SessionsApi, UsersApi } from "../api";
+import type {
+  AssetsApi,
+  ProjectConfigRspAllOfData,
+  ProjectsApi,
+  SessionsApi,
+  UsersApi,
+} from "../api";
 
 export type CookiesDefinition = {
   name: string;
@@ -16,3 +22,11 @@ export interface IApiService {
   projectsApi: ProjectsApi;
   sessionsApi: SessionsApi;
 }
+
+export type StepFunctionParams = number | string | boolean;
+export type StepFunction = (
+  projectCOnfig: ProjectConfigRspAllOfData,
+  ...args: StepFunctionParams[]
+) => string;
+export type Flow = Record<string, StepFunction>;
+export type Flows = Record<string, Flow>;

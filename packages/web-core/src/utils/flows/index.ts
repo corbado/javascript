ed82819de998255bb107b5e140e06fdbac771496
Abdullah Shahbaz <abdullah_ghani@live.com>
2023-11-04 11:26:10 +0500

    fixed types isssue in web-core

--- packages/web-core/src/utils/flows/index.ts
+++ packages/web-core/src/utils/flows/index.ts
@@ -1,4 +1,4 @@
-import type { Flows } from "../../types/common";
+import type { Flows } from "../../types";
 import { EmailOTPSignupFlow } from "./EmailOTPSignup";
 import { PasskeyLoginWithEmailOTPFallbackFlow } from "./PasskeyLoginWithEmailOTPFallback";
 import { PasskeySignupWithEmailOTPFallbackFlow } from "./PasskeySignupWithEmailOTPFallback";

dae965a86f0d63f6e3dd9c5faf50721273adf421
Abdullah Shahbaz <abdullah_ghani@live.com>
2023-11-04 11:31:26 +0500

    update emailotpsignup flow to support start and stop

--- packages/web-core/src/utils/flows/EmailOTPSignup.ts
+++ packages/web-core/src/utils/flows/EmailOTPSignup.ts
@@ -1,3 +1,7 @@
+//import type { ProjectConfigRspAllOfData } from "../../api";
 import type { Flow } from "../../types";
 
-export const EmailOTPSignupFlow: Flow = {};
+export const EmailOTPSignupFlow: Flow = {
+  start: () => "enter-otp",
+  "enter-otp": () => "end",
+};

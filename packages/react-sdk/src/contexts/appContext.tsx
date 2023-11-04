88ed434a8429d031e5949f15e62c1edcfcddc833
Abdullah Shahbaz <abdullah_ghani@live.com>
2023-11-04 11:32:04 +0500

    added flowhandler context

--- packages/react-sdk/src/contexts/appContext.tsx
+++ packages/react-sdk/src/contexts/appContext.tsx
@@ -1,22 +1,64 @@
-import type { IApiService, ProjectConfigRspAllOfData } from "@corbado/web-core";
-import { ApiService, defaultTimeout } from "@corbado/web-core";
-import React, { createContext, type FC, useEffect, useState } from "react";
+import type {
+  IApiService,
+  IFlowHandlerConfig,
+  LoginFlowNames,
+  ProjectConfigRspAllOfData,
+  SignUpFlowNames,
+} from "@corbado/web-core";
+import {
+  ApiService,
+  defaultTimeout,
+  FlowHandlerService,
+} from "@corbado/web-core";
+import React, {
+  createContext,
+  type FC,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 export interface IAppContext {
   apiService: IApiService;
+  flowHandlerService: FlowHandlerService | null;
   projectConfig: ProjectConfigRspAllOfData;
+}
+
+export interface IAppProviderParams extends IFlowHandlerConfig {
+  projectId: string;
+  apiTimeout?: number;
+  defaultToLogin: boolean;
+  signupFlowName: SignUpFlowNames;
+  loginFlowName: LoginFlowNames;
 }
 
 export const AppContext = createContext<IAppContext | null>(null);
 
-export const AppProvider: FC<{
-  projectId: string;
-  apiTimeout: number;
-}> = ({ projectId, apiTimeout = defaultTimeout, children }) => {
-  const apiService = ApiService(projectId, apiTimeout);
+export const AppProvider: FC<IAppProviderParams> = ({
+  projectId,
+  apiTimeout = defaultTimeout,
+  defaultToLogin,
+  signupFlowName,
+  loginFlowName,
+  passkeyAppend,
+  retryPasskeyOnError,
+  compulsoryEmailVerification,
+  shouldRedirect,
+  children,
+}) => {
+  const flowHandlerConfig = {
+    passkeyAppend,
+    retryPasskeyOnError,
+    compulsoryEmailVerification,
+    shouldRedirect,
+  };
+  const apiServiceRef = useRef(ApiService(projectId, apiTimeout));
+  const apiService = apiServiceRef.current;
   const [projectConfig, setProjectConfig] = useState(
     {} as ProjectConfigRspAllOfData
   );
+  const flowHandlerServiceRef = useRef<FlowHandlerService | null>(null);
+  const flowHandlerService = flowHandlerServiceRef.current;
 
   useEffect(() => {
     apiService.projectsApi
@@ -25,14 +67,21 @@ export const AppProvider: FC<{
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const response = (config.data as any).data as ProjectConfigRspAllOfData;
         setProjectConfig(response);
+        flowHandlerServiceRef.current = new FlowHandlerService(
+          defaultToLogin ? loginFlowName : signupFlowName,
+          projectConfig,
+          flowHandlerConfig
+        );
       })
       .catch((error) => {
         console.error(error);
       });
-  }, [apiService]);
+  }, []);
 
   return (
-    <AppContext.Provider value={{ apiService, projectConfig }}>
+    <AppContext.Provider
+      value={{ apiService, projectConfig, flowHandlerService }}
+    >
       {children}
     </AppContext.Provider>
   );
--- packages/react-sdk/src/contexts/index.tsx
+++ packages/react-sdk/src/contexts/index.tsx
@@ -1,20 +1,46 @@
+import type {
+  IFlowHandlerConfig,
+  LoginFlowNames,
+  SignUpFlowNames,
+} from "@corbado/web-core";
+import { defaultTimeout } from "@corbado/web-core";
 import type { FC } from "react";
 import React from "react";
 
 import { AppProvider } from "./appContext";
 
-export interface ICorbadoContextParams {
+export interface ICorbadoContextParams extends IFlowHandlerConfig {
   projectId: string;
-  apiTimeout: number;
+  apiTimeout?: number;
+  defaultToLogin: boolean;
+  signupFlowName: SignUpFlowNames;
+  loginFlowName: LoginFlowNames;
 }
 
 export const CorbadoProvider: FC<ICorbadoContextParams> = ({
   projectId,
-  apiTimeout,
+  apiTimeout = defaultTimeout,
+  defaultToLogin,
+  signupFlowName,
+  loginFlowName,
+  passkeyAppend,
+  retryPasskeyOnError,
+  compulsoryEmailVerification,
+  shouldRedirect,
   children,
 }) => {
   return (
-    <AppProvider projectId={projectId} apiTimeout={apiTimeout}>
+    <AppProvider
+      projectId={projectId}
+      apiTimeout={apiTimeout}
+      defaultToLogin={defaultToLogin}
+      signupFlowName={signupFlowName}
+      loginFlowName={loginFlowName}
+      passkeyAppend={passkeyAppend}
+      retryPasskeyOnError={retryPasskeyOnError}
+      compulsoryEmailVerification={compulsoryEmailVerification}
+      shouldRedirect={shouldRedirect}
+    >
       {children}
     </AppProvider>
   );

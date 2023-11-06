40aa7198d3924aa8bb4346beb2fc9727d69229d2
Abdullah Shahbaz <abdullah_ghani@live.com>
2023-11-06 20:59:18 +0500

    updated useCorbadoAppContext hook

--- packages/react-sdk/src/hooks/useCorbadoAppContext.ts
+++ packages/react-sdk/src/hooks/useCorbadoAppContext.ts
@@ -1,11 +1,13 @@
 import { useContext } from "react";
 
-import { AppContext } from "../contexts/appContext";
+import { AppContext } from "../contexts/CorbadoAppContext";
 
 export const useAppContext = () => {
   const context = useContext(AppContext);
   if (!context) {
     throw new Error("useAppContext must be used within an AppProvider");
   }
-  return context;
+  return {
+    projectConfig: context.projectConfig,
+  };
 };

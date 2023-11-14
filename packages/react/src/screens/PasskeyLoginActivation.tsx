47f9a9fce066e4d05afabdea26198ef0738f5185
Isah Ohieku <isahohieku@gmail.com>
2023-11-14 19:33:08 +0100

    complete login passkey activation screen

--- packages/react/src/screens/PasskeyLoginActivation.tsx
+++ packages/react/src/screens/PasskeyLoginActivation.tsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import { useCorbadoAuth, useCorbadoFlowHandler } from '@corbado/react-sdk';
+import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 
-import type { ButtonType} from '../components/PasscodeScreensWrapper';
-import { PasscodeScreensWrapper } from '../components/PasscodeScreensWrapper';
+import type { ButtonType } from '../components/PasskeyScreensWrapper';
+import { PasskeyScreensWrapper } from '../components/PasskeyScreensWrapper';
 
 export const PasskeyLoginActivation = () => {
     const { t } = useTranslation();
+    const { navigateToNextScreen } = useCorbadoFlowHandler();
+    const { passkeyAppend } = useCorbadoAuth();
+
+
+    const showBenefit = () => void navigateToNextScreen({ showBenefit: true });
 
     const header = <Trans i18nKey="activate_passkey.header">
-        text <span className="text-primary-color underline">x</span>
+        text <span onClick={showBenefit} className="text-primary-color underline">x</span>
     </Trans>;
 
     const primaryButton = t('activate_passkey.primary_btn');
     const secondaryButton = t('activate_passkey.secondary_btn');
 
-    const handlePasskeyActivation = () => console.log('Activate passkey login');
-    const handleLater = () => console.log('Maybe later');
+    const appendPasskey = async () => {
+        try {
+            const response = await passkeyAppend();
+            if (response) {
+                await navigateToNextScreen({ success: true });
+                return;
+            }
+            await navigateToNextScreen({ failure: false });
+        } catch (error) {
+            console.log(error);
+            await navigateToNextScreen({ failure: true });
+        }
+    }
+
+    const handlePasskeyActivation = () => void appendPasskey();
+    const handleLater = () => void navigateToNextScreen({ maybeLater: true });
 
     const handleClick = (btn: ButtonType) => {
 
@@ -34,7 +54,7 @@ export const PasskeyLoginActivation = () => {
 
     return (
         <>
-            <PasscodeScreensWrapper
+            <PasskeyScreensWrapper
                 {...props}
             />
         </>

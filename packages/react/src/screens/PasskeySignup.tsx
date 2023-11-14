c71c6e3c98e72ef534283b48ab125058b96001bf
Isah Ohieku <isahohieku@gmail.com>
2023-11-14 19:31:43 +0100

    Complete passkey register screen

--- packages/react/src/screens/PasskeySignup.tsx
+++ packages/react/src/screens/PasskeySignup.tsx
@@ -1,32 +1,51 @@
+import { useCorbadoAuth, useCorbadoFlowHandler } from '@corbado/react-sdk';
 import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 
-import type { ButtonType} from '../components/PasscodeScreensWrapper';
-import { PasscodeScreensWrapper } from '../components/PasscodeScreensWrapper';
+import type { ButtonType } from '../components/PasskeyScreensWrapper';
+import { PasskeyScreensWrapper } from '../components/PasskeyScreensWrapper';
 
 export const PasskeySignup = () => {
     const { t } = useTranslation();
 
-    const email_address = 'janina+test125@corbado.com';
+    const { navigateToNextScreen, navigateBack } = useCorbadoFlowHandler();
+    const { passkeyRegister, getEmail } = useCorbadoAuth();
+
+    const email = getEmail();
+
+    const showBenefit = () => void navigateToNextScreen({ showBenefit: true })
 
     const header = <Trans i18nKey="passkey_signup.header">
-        text <span className="text-primary-color underline">x</span> text
+        text <span onClick={showBenefit} className="text-primary-color underline">x</span> text
     </Trans>;
 
     const subHeader = <Trans i18nKey="passkey_signup.sub-header">
-        text <span className="text-secondary-font-color">{{email_address} as any}</span> text
+        text <span className="text-secondary-font-color">{{ email } as any}</span> text
     </Trans>;
 
     const primaryButton = t('passkey_signup.primary_btn');
     const secondaryButton = t('passkey_signup.secondary_btn');
     const tertiaryButton = t('passkey_signup.tertiary_btn');
 
-    const handleCreateAccount = () => console.log('Create Account with Passkey');
-    const handleSendOtp = () => console.log('Send email OTP');
-    const handleBack = () => console.log('Go Back');
+    const createPasskey = async () => {
+        try {
+            const response = await passkeyRegister();
+            if (response) {
+                await navigateToNextScreen({ success: true });
+                return;
+            }
+            await navigateToNextScreen({ failure: false });
+        } catch (error) {
+            console.log(error);
+            await navigateToNextScreen({ failure: true });
+        }
+    }
+
+    const handleCreateAccount = () => void createPasskey();
+    const handleSendOtp = () => void navigateToNextScreen({ sendOtpEmail: true });
+    const handleBack = () => navigateBack();
 
     const handleClick = (btn: ButtonType) => {
-
         if (btn === 'primary') {
             return handleCreateAccount();
         }
@@ -48,7 +67,7 @@ export const PasskeySignup = () => {
 
     return (
         <>
-            <PasscodeScreensWrapper
+            <PasskeyScreensWrapper
                 {...props}
             />
         </>

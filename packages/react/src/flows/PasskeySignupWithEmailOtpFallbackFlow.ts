df815191a53a725799319f56f0e167f4b5215640
Isah Ohieku <isahohieku@gmail.com>
2023-11-14 19:34:14 +0100

    update passkey signup screens list

--- packages/react/src/flows/PasskeySignupWithEmailOtpFallbackFlow.ts
+++ packages/react/src/flows/PasskeySignupWithEmailOtpFallbackFlow.ts
@@ -1,17 +1,21 @@
-import { PasskeySignupWithEmailOtpFallbackScreens } from '@corbado/web-core'
 
+import { PasskeySignupWithEmailOtpFallbackScreens } from '@corbado/react-sdk';
+
+import { CreatePasskeyBenefit } from '../screens/CreatePasskeyBenefit';
 import { EmailLink } from '../screens/EmailLink';
 import { InitiateSignup } from '../screens/InitiateSignup';
 import { PasskeyCreationError } from '../screens/PasskeyCreationError';
 import { PasskeyCreationSuccess } from '../screens/PasskeyCreationSuccess';
-
-import type { ScreensList } from '../types';
+import { PasskeyLoginActivation } from '../screens/PasskeyLoginActivation';
 import { PasskeySignup } from '../screens/PasskeySignup';
+import type { ScreensList } from '../types';
 
 export const PasskeySignupWithEmailOtpFallbackFlow: ScreensList = {
   [PasskeySignupWithEmailOtpFallbackScreens.Start]: InitiateSignup, 
   [PasskeySignupWithEmailOtpFallbackScreens.CreatePasskey]: PasskeySignup, 
+  [PasskeySignupWithEmailOtpFallbackScreens.PasskeyBenefits]: CreatePasskeyBenefit, 
   [PasskeySignupWithEmailOtpFallbackScreens.EnterOtp]: EmailLink,
   [PasskeySignupWithEmailOtpFallbackScreens.PasskeyWelcome]: PasskeyCreationSuccess,
   [PasskeySignupWithEmailOtpFallbackScreens.PasskeyError]: PasskeyCreationError,
+  [PasskeySignupWithEmailOtpFallbackScreens.PasskeyAppend]: PasskeyLoginActivation,
 };

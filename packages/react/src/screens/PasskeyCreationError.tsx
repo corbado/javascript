f1be894d875bbff1a071c7d97243418ca6d8b71f
Isah Ohieku <isahohieku@gmail.com>
2023-11-14 19:32:31 +0100

    complete passkey creation error screen

--- packages/react/src/screens/PasskeyCreationError.tsx
+++ packages/react/src/screens/PasskeyCreationError.tsx
@@ -1,24 +1,28 @@
+import { useCorbadoFlowHandler } from '@corbado/react-sdk';
 import React from 'react'
 import { Trans, useTranslation } from 'react-i18next';
 
-import type { ButtonType} from '../components/PasscodeScreensWrapper';
-import { PasscodeScreensWrapper } from '../components/PasscodeScreensWrapper';
+import type { ButtonType} from '../components/PasskeyScreensWrapper';
+import { PasskeyScreensWrapper } from '../components/PasskeyScreensWrapper';
 
 export const PasskeyCreationError = () => {
     const { t } = useTranslation();
+    const { navigateToNextScreen, navigateBack } = useCorbadoFlowHandler();
+
+    const showBenefit = () => void navigateToNextScreen({ showBenefit: true })
 
     const header = t('create_passkey_error.header');
     const body = <Trans i18nKey="create_passkey_error.body">
-        Creating your account with <strong className='text-primary-color underline'>passkeys</strong> not possible. Try again or log in with email one time code.
+        Creating your account with <strong onClick={showBenefit} className='text-primary-color underline'>passkeys</strong> not possible. Try again or log in with email one time code.
     </Trans>;
 
     const primaryButton = t('create_passkey_error.primary_btn');
     const secondaryButton = t('create_passkey_error.secondary_btn');
     const tertiaryButton = t('create_passkey_error.tertiary_btn');
 
-    const handleCreatePasskey = () => console.log('Try again');
-    const handleSendOtp = () => console.log('Send email OTP');
-    const handleBack = () => console.log('Go back');
+    const handleCreatePasskey = () => void navigateToNextScreen({ success: true });
+    const handleSendOtp = () => void navigateToNextScreen({ sendOtpEmail: true });
+    const handleBack = () => navigateBack();
 
     const handleClick = (btn: ButtonType) => {
         if (btn === 'primary') {
@@ -41,7 +45,7 @@ export const PasskeyCreationError = () => {
 
     return (
         <>
-            <PasscodeScreensWrapper
+            <PasskeyScreensWrapper
                 {...props}
             />
         </>

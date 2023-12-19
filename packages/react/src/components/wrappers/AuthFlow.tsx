1b8d55185eb12802f26b081f4a9103c8675219c8
Abdullah Shahbaz <abdullah_ghani@live.com>
2023-12-19 20:42:05 +0500

    fixed local build issue

--- packages/react/src/components/wrappers/AuthFlow.tsx
+++ packages/react/src/components/wrappers/AuthFlow.tsx
@@ -1,11 +1,12 @@
 import { CommonScreens } from '@corbado/shared-ui';
+import type { FC } from 'react';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import type { ScreenMap } from '../../flows';
 import { flowScreensMap } from '../../flows';
 import useFlowHandler from '../../hooks/useFlowHandler';
 
-export const AuthFlow = () => {
+export const AuthFlow: FC = () => {
   const { currentFlow, currentScreen } = useFlowHandler();
   const [ComponentMap, setComponentMap] = useState<ScreenMap>({});
 

44e8c77dc9be57b96bcc0be652dc1370d0dbd625
Abdullah Shahbaz <abdullah_ghani@live.com>
2023-12-27 10:56:38 +0500

    Delete unused react-custom-ui example

--- playground/react-custom-ui/src/pages/PasskeyLoginTest.jsx
+++
@@ -1,9 +0,0 @@
-import { useCorbadoFlowHandler } from '@corbado/react-sdk';
-import React from 'react';
-import { PasskeyLoginWithEmailOtpFallback } from '../flows/PasskeyLoginWithEmailOtpFallback';
-
-export function PasskeyLoginTest() {
-  const { currentScreenName } = useCorbadoFlowHandler();
-
-  return <div>{PasskeyLoginWithEmailOtpFallback[currentScreenName]}</div>;
-}
--- playground/react-custom-ui/src/pages/SingUpWithEmailOtpTest.jsx
+++
@@ -1,9 +0,0 @@
-import { useCorbadoFlowHandler } from '@corbado/react-sdk';
-import React from 'react';
-import { SignupWithEmailOtpFlows } from '../flows/SignupWithEmailOtp';
-
-export function SignUpWithEmailOtpTest() {
-  const { currentScreenName } = useCorbadoFlowHandler();
-
-  return <div>{SignupWithEmailOtpFlows[currentScreenName]}</div>;
-}

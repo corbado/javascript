9d958d19f864d8d143e83f4c4fd78016da19263a
Anders Choi <anders.choi@corbado.com>
2024-01-23 18:26:19 +0100

    prettier

--- packages/tests-e2e/src/utils/helperFunctions/setWebAuthnAutomaticPresenceSimulation.ts
+++ packages/tests-e2e/src/utils/helperFunctions/setWebAuthnAutomaticPresenceSimulation.ts
@@ -1,6 +1,10 @@
 import type { CDPSession } from '@playwright/test';
 
-export async function setWebAuthnAutomaticPresenceSimulation(client: CDPSession, authenticatorId: string, automatic: boolean) {
+export async function setWebAuthnAutomaticPresenceSimulation(
+  client: CDPSession,
+  authenticatorId: string,
+  automatic: boolean,
+) {
   await client.send('WebAuthn.setAutomaticPresenceSimulation', {
     authenticatorId: authenticatorId,
     enabled: automatic,

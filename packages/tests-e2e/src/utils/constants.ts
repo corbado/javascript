70ade9fb9518a2da9925e1ffa449f0086dfe6613
Anders Choi <anders.choi@corbado.com>
2024-01-23 22:48:38 +0100

    increase timeout even more

--- packages/tests-e2e/src/utils/constants.ts
+++ packages/tests-e2e/src/utils/constants.ts
@@ -14,4 +14,4 @@ export enum OtpType {
   Incorrect = '150918',
 }
 
-export const waitAfterLoad = 500; // remove after repetitive refreshing is fixed
+export const waitAfterLoad = 600; // remove after repetitive refreshing is fixed

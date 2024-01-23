4741e6cddf277a77e67779d9986dd8f0554184ef
Anders Choi <anders.choi@corbado.com>
2024-01-23 15:19:58 +0100

    add test for conditional UI

--- packages/tests-e2e/src/ui/chromium/login/Passkey-functionality.spec.ts
+++ packages/tests-e2e/src/ui/chromium/login/Passkey-functionality.spec.ts
@@ -46,9 +46,40 @@ test.describe('Login with passkey proper user behavior', () => {
     await loginFlow.checkPasskeyRegistered();
   });
 
-  // test('from Start with conditional UI', async ({ loginFlow, page }) => {
+  test('from Start with conditional UI', async ({ loginFlow, page }) => {
+    await loginFlow.initializeCDPSession();
+    await loginFlow.addWebAuthn(true);
+    await loginFlow.loadAuth();
 
-  // });
+    const [, email] = await loginFlow.createAccount(true);
+
+    await loginFlow.navigateToStartScreen();
+
+    await loginFlow.setWebAuthnAutomaticPresenceSimulation(false);
+    await page.getByPlaceholder('Email address').click();
+    await page.getByPlaceholder('Email address').fill(email);
+    await expect(page.getByPlaceholder('Email address')).toHaveValue(email);
+    await page.getByRole('button', { name: 'Continue' }).click();
+    await loginFlow.setWebAuthnAutomaticPresenceSimulation(true);
+
+    // first login requires OTP
+    await loginFlow.checkLandedOnScreen(ScreenNames.EnterOtp);
+    await loginFlow.fillOTP();
+    await loginFlow.checkLandedOnScreen(ScreenNames.End);
+    await loginFlow.checkPasskeyRegistered();
+
+    await page.getByRole('button', { name: 'Logout' }).click();
+
+    await loginFlow.loadAuth();
+    await loginFlow.navigateToStartScreen();
+
+    // Conditional UI shows up when email textbox is selected (and automatically processed)
+    await page.getByPlaceholder('Email address').click();
+
+    // second login does not require OTP
+    await loginFlow.checkLandedOnScreen(ScreenNames.End);
+    await loginFlow.checkPasskeyRegistered();
+  });
 
   test('from PasskeyAppend', async ({ loginFlow, page }) => {
     await loginFlow.initializeCDPSession();

8cc87973c6ab6eeec9191d785fba71c92f3a9e7b
Anders Choi <anders.choi@corbado.com>
2024-01-29 15:07:01 +0100

    add a small timeout to be sure

--- packages/tests-e2e/src/utils/constants.ts
+++ packages/tests-e2e/src/utils/constants.ts
@@ -14,4 +14,4 @@ export enum OtpType {
   Incorrect = '150918',
 }
 
-export const waitAfterLoad = 0; // adding a timeout after each page load reduces flakiness (add if flakiness becomes a problem in the pipeline)
+export const waitAfterLoad = 100; // timeout after each page load reduces flakiness (increase if flakiness becomes a problem in the pipeline)

318de9d91446dee96f6da01d9cc2d5fb3ea4b9c4
Abdullah Shahbaz <abdullah_ghani@live.com>
2024-02-13 19:47:34 +0500

    updated current user to user full user details to test node-sdk api calls

--- examples/nextjs/app/ui/dashboard/current-user.tsx
+++ examples/nextjs/app/ui/dashboard/current-user.tsx
@@ -1,36 +1,44 @@
-import { Config, SDK } from '@corbado/node-sdk';
 import { cookies } from 'next/headers';
 import { CheckCircleIcon, XCircleIcon, UserCircleIcon, EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline';
+import createNodeSDK from '@/app/utils/createNodeSDK';
 
 export default async function CurrentUser() {
   const cookieStore = cookies();
   const session = cookieStore.get('cbo_short_session');
-  const projectID = process.env.NEXT_PUBLIC_CORBADO_PROJECT_ID!;
-  const apiSecret = process.env.CORBADO_API_SECRET!;
-
-  const config = new Config(projectID, apiSecret);
-  const sdk = new SDK(config);
-
-  const users = await sdk.sessions().getCurrentUser(session?.value ?? '');
+  const sdk = createNodeSDK();
+  const currentSessionUser = await sdk.sessions().getCurrentUser(session?.value ?? '');
+  const userResp = await sdk.users().get(currentSessionUser?.getID() ?? '');
+  const user = userResp.data;
+  const activeEmail = user.emails.find(email => email.status === 'active');
+  const activePhone = user.phoneNumbers.find(phone => phone.status === 'active');
 
   return (
     <>
-      <h1 className='text-2xl font-bold'>Hi {users.getName()},</h1>
+      <h1 className='text-2xl font-bold'>Hi {user.fullName},</h1>
       <div className='mb-3 mt-3'>
         <p>These are your user details:</p>
         <ul className='m-3 flex flex-col gap-3'>
           <li className='flex'>
-            <EnvelopeIcon className='w-6 mr-2' /> Email: {users.getEmail()}
+            <EnvelopeIcon className='w-6 mr-2' /> Email:{' '}
+            {activeEmail ? `${activeEmail?.email} create at ${activeEmail?.created}` : 'No active email'}
           </li>
           <li className='flex'>
-            <UserCircleIcon className='w-6 mr-2' /> Name: {users.getName()}
+            <UserCircleIcon className='w-6 mr-2' /> Name: {user.fullName}
           </li>
           <li className='flex'>
-            <PhoneIcon className='w-6 mr-2' /> Phone Number: {users.getPhoneNumber()}
+            <PhoneIcon className='w-6 mr-2' /> Phone Number:{' '}
+            {activePhone?.phoneNumber
+              ? `${activePhone?.phoneNumber} created at ${activePhone?.created}`
+              : 'No active phone number'}
+            {activePhone?.created}
           </li>
           <li className='flex gap-2'>
             Is Authenticated:{' '}
-            {users.isAuthenticated() ? <CheckCircleIcon className='w-6' /> : <XCircleIcon className='w-6' />}
+            {currentSessionUser.isAuthenticated() ? (
+              <CheckCircleIcon className='w-6' />
+            ) : (
+              <XCircleIcon className='w-6' />
+            )}
           </li>
         </ul>
       </div>

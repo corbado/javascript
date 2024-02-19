ad276fc4125ac80470627cc7fea79c79d123a7fa
Abdullah Shahbaz <abdullah_ghani@live.com>
2024-02-19 17:58:23 +0500

    added badge in login and signup components

--- packages/react/src/screens/core/SignUp.tsx
+++ packages/react/src/screens/core/SignUp.tsx
@@ -3,19 +3,28 @@ import type { CorbadoSignUpConfig } from '@corbado/types';
 import type { FC } from 'react';
 import React from 'react';
 
-import { AuthFlow } from '../../components';
+import { AuthFlow, FreemiumBadge } from '../../components';
 import FlowHandlerProvider from '../../contexts/FlowHandlerProvider';
 
 const SignUp: FC<CorbadoSignUpConfig> = ({ onSignedUp, navigateToLogin }) => {
   return (
-    <div className='cb-container'>
-      <FlowHandlerProvider
-        onLoggedIn={onSignedUp}
-        onChangeFlow={navigateToLogin}
-        initialFlowType={FlowType.SignUp}
-      >
-        <AuthFlow />
-      </FlowHandlerProvider>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <div className='cb-container'>
+        <FlowHandlerProvider
+          onLoggedIn={onSignedUp}
+          onChangeFlow={navigateToLogin}
+          initialFlowType={FlowType.SignUp}
+        >
+          <AuthFlow />
+        </FlowHandlerProvider>
+      </div>
+      <FreemiumBadge />
     </div>
   );
 };

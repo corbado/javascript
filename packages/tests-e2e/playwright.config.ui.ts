984b522320a22424c5d1b15be6493e8efda4b4b9
Anders Choi <anders.choi@corbado.com>
2024-02-20 16:14:37 +0100

    increase number of workers

--- packages/tests-e2e/playwright.config.ui.ts
+++ packages/tests-e2e/playwright.config.ui.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  // workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? process.env.PLAYWRIGHT_NUM_WORKERS : undefined,
   reporter: 'html',
   timeout: 15000, // default: 30000ms
   expect: {

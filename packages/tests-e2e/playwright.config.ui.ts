6dbbc8bfbca13831d17cc0e6b23985725af894cf
Anders <anders.choi@corbado.com>
2024-02-22 10:03:02 +0100

    Optimize playwright tests to run on self-hosted runners, fix some broken tests (#205)
    * fix some tests, lower timeout, change runner
    
    * deploy playground for this branch
    
    * try another label
    
    * prettier
    
    * specify vm1 runner
    
    * try changing runner name
    
    * remove comment
    
    * make .yml same as in corbado repo..
    
    * specify vm1
    
    * check if different syntax works
    
    * revert to using label instead of specific runner name
    
    * gratn permissions for lerna
    
    * debug log
    
    * debug log
    
    * debug log
    
    * debug
    
    * eyo
    
    * compress and extract node_modules to preserve permissions
    
    * fix path
    
    * add caching
    
    * increase number of workers
    
    * fix number of workers
    
    * try 7 workers
    
    * num_workers = num_cores - 1
    
    * add action and navigation timeouts
    
    * fix number of cores
    
    * change github runner label just for testing
    
    * revert runner label
    
    * add comments
    
    * fix github actions branches
    
    * try out maximum number of workers
    
    * slightly increase the action/navigation timeout
    
    * revert to workers = cores - 1
    
    * clean install vercel deployment, revert timeout changes
    
    * revert deploy playground github actions
    
    * add comments
    
    * modify comment

--- packages/tests-e2e/playwright.config.ui.ts
+++ packages/tests-e2e/playwright.config.ui.ts
@@ -5,9 +5,23 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  // workers: process.env.CI ? 1 : undefined,
+  // Using all cores increases flakiness due to slow browsers.
+  // Recommended # of workers = # of cores / 2, but leaving only 1 free
+  // core seems enough to prevent flakiness on our self-hosted runners.
+  // Ref: https://learn.microsoft.com/en-us/azure/playwright-testing/concept-determine-optimal-configuration#run-tests-locally
+  workers: process.env.CI
+    ? process.env.PLAYWRIGHT_NUM_CORES
+      ? parseInt(process.env.PLAYWRIGHT_NUM_CORES, 10) - 1
+      : undefined
+    : undefined,
   reporter: 'html',
+  timeout: 15000, // default: 30000ms
+  expect: {
+    timeout: 3000, // default: 5000ms
+  },
   use: {
+    actionTimeout: 3000, // default: none
+    navigationTimeout: 3000, // default: none
     baseURL: process.env.PLAYWRIGHT_TEST_URL,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',

4cdfa9dbe920754935f185bcf930b82775a69b77
Abdullah Shahbaz <abdullah_ghani@live.com>
2024-03-01 17:58:50 +0500

    removed extra code

--- playground/react/src/pages/HomePage.tsx
+++ playground/react/src/pages/HomePage.tsx
@@ -2,46 +2,9 @@ import { useCorbado } from '@corbado/react';
 import withCorbadoProvider from '../hoc/withCorbadoProvider';
 import { AuthDetails } from '../components/AuthDetails';
 import { AuthButtons } from '../components/AuthButtons';
-// import './temp.css';
-// import Logo from '../assets/logo.svg';
-// import SignUpForm from '../components_temp/SignUpFrom';
 
 const HomePage = () => {
   const { isAuthenticated } = useCorbado();
-
-  // return (
-  //   <>
-  //     <div className='cb-container'>
-  //       <div className='cb-header'>Create your account</div>
-  //       <div className='cb-subheader'>to continue to AppName</div>
-
-  //       <div>
-  //         <label className='cb-input-label'>Username</label>
-  //         <input
-  //           type='text'
-  //           className='cb-input-field'
-  //         />
-  //       </div>
-
-  //       <div className='cb-input-field'>
-  //         <label className='cb-input-label'>Email address</label>
-  //         <input type='email' />
-  //       </div>
-
-  //       <button className='cb-continue-button'>Continue</button>
-
-  //       <div className='cb-login-link'>
-  //         You already have an account?{' '}
-  //         <a
-  //           href='#'
-  //           className='cb-login-link-text'
-  //         >
-  //           Log in
-  //         </a>
-  //       </div>
-  //     </div>
-  //   </>
-  // );
 
   return isAuthenticated ? <AuthDetails /> : <AuthButtons />;
 };

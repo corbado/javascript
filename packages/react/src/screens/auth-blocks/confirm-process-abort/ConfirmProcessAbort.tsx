2eb2d05e3b2c65b9d12eee8fdb9481e76ef1ccc7
Incorbador <incorbador@gmx.de>
2024-03-21 17:13:04 +0100

    ConfirmProcessAbort styling

--- packages/react/src/screens/auth-blocks/confirm-process-abort/ConfirmProcessAbort.tsx
+++ packages/react/src/screens/auth-blocks/confirm-process-abort/ConfirmProcessAbort.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 
 import { PrimaryButton } from '../../../components/ui2/buttons/PrimaryButton';
 import { SecondaryButton } from '../../../components/ui2/buttons/SecondaryButton';
+import { FirstPageIcon } from '../../../components/ui2/icons/FirstPageIcon';
 import { Header } from '../../../components/ui2/typography/Header';
 import { Text } from '../../../components/ui2/typography/Text';
 
@@ -19,6 +20,9 @@ export const ConfirmProcessAbort = ({ block }: { block: ConfirmProcessAbortBlock
       >
         {t('subheader')}
       </Text>
+      <div className='cb-confirm-process-abort-container'>
+        <FirstPageIcon className='cb-confirm-process-abort-icon' />
+      </div>
       <PrimaryButton onClick={() => void block.confirmAbort()}>{t('confirm')}</PrimaryButton>
       <SecondaryButton onClick={() => block.cancelAbort(block.data)}>{t('back')}</SecondaryButton>
     </div>

2874ca07f1da2f92ab42afb4e087491b2bd5a597
Abdullah Shahbaz <abdullah_ghani@live.com>
2024-04-03 03:30:55 +0500

    updated demo react application

--- examples/react/src/translations/enCustomized.ts
+++ examples/react/src/translations/enCustomized.ts
@@ -1,22 +1,24 @@
 const en = {
-  authentication: {
-    signup: {
-      start: {
-        header: "Let's create an account to check Corbado Test App",
-        subheader: 'Would you like to login? ',
-        button_login: 'Log in',
+  signup: {
+    'signup-init': {
+      'signup-init': {
+        header: "Let's create an account",
+        subheader: 'to check ',
+        text_login: 'Would you like to login? ',
         button_submit: 'Sign up',
-        textField_name: 'Full name',
-        textField_email: 'Username',
+        textField_fullName: 'Full Name',
+        text_divider: 'or use social logins',
       },
     },
-    login: {
-      start: {
-        header: 'Please login to check Corbado Test App',
-        subheader: 'Would you like to create an account? ',
+  },
+  login: {
+    'login-init': {
+      'login-init': {
+        header: 'Please login',
+        subheader: 'to check ',
+        text_signup: 'Would you like to create an account? ',
         button_signup: 'Sign up',
         button_submit: 'Login',
-        textField_email: 'Username',
       },
     },
   },

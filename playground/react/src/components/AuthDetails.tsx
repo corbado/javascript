0078fb603366b2b4bb2b8de60b21cc95f734f87e
Abdullah Shahbaz <abdullah_ghani@live.com>
2024-04-03 17:39:29 +0500

    added async logout

--- playground/react/src/components/AuthDetails.tsx
+++ playground/react/src/components/AuthDetails.tsx
@@ -14,7 +14,7 @@ export const AuthDetails = () => {
         <UserDetails />
         <button
           onClick={async () => {
-            logout();
+            await logout();
 
             // TODO: this should be covered by a guard (then we can remove it)
             navigate(`/${projectId}/auth`);

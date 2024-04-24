1836ec4080962b344e0d3dde23ab73f5aab663c2
Anders Choi <anders.choi@corbado.com>
2024-04-24 16:35:30 +0200

    fix core api url

--- packages/tests-e2e/cleanup.sh
+++ packages/tests-e2e/cleanup.sh
@@ -8,7 +8,7 @@ function iterate() {
     for file in "$dir"/*; do
         if [ -f "$file" ]; then
             PROJECT_ID=`cat "$file" | xargs`
-            curl --location --request DELETE 'https://app.corbado-dev.com/v1/projects' \
+            curl --location --request DELETE 'https://app.corbado.com/v1/projects' \
                 --header 'Content-Type: text/plain' \
                 --header 'Cookie: cbo_short_session='"$PLAYWRIGHT_JWT_TOKEN"'' \
                 --data '{ "id": "'"$PROJECT_ID"'" }'

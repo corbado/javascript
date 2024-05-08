2cbd09778d091d8e85fcd6a6dee8e94dd4837b39
Anders <anders.choi@corbado.com>
2024-05-08 18:18:27 +0200

    Split e2e tests into commitly and nightly tests (#342)
    * delete empty tests
    
    * add commitly and nightly test groups to config file
    
    * prettier
    
    * test new cron job
    
    * try new time
    
    * edit outer package.json
    
    * change cron time
    
    * change cron time
    
    * change workflow name
    
    * resolve merge conflict problem
    
    * formatting
    
    * revert time
    
    * reduce chance of running it during high loads of github workflow runs
    
    * reorganize playwright project structure to be more intuitive
    
    * prettier

--- packages/tests-e2e/src/utils/StateManager.ts
+++ packages/tests-e2e/src/utils/StateManager.ts
@@ -1,22 +1,33 @@
 import { existsSync, mkdirSync, readFileSync, rmSync, writeFileSync } from 'fs';
 
 class StateManager {
+  private static makeKey(playwrightProjectName: string) {
+    if (playwrightProjectName.includes('setup') || playwrightProjectName.includes('teardown')) {
+      return playwrightProjectName.split('-').slice(0, -1).join('-');
+    } else {
+      return playwrightProjectName.split('-').slice(0, -2).join('-');
+    }
+  }
+
   public static setProjectId(playwrightProjectName: string, projectId: string) {
     if (!existsSync(__dirname + '/../../test-states')) {
       mkdirSync(__dirname + '/../../test-states');
     }
-    const key = playwrightProjectName.split('-').slice(0, -1).join('-');
-    writeFileSync(__dirname + `/../../test-states/${key}.txt`, projectId, { encoding: 'utf-8', flag: 'w' });
+    writeFileSync(__dirname + `/../../test-states/${StateManager.makeKey(playwrightProjectName)}.txt`, projectId, {
+      encoding: 'utf-8',
+      flag: 'w',
+    });
   }
 
   public static getProjectId(playwrightProjectName: string) {
-    const key = playwrightProjectName.split('-').slice(0, -1).join('-');
-    return readFileSync(__dirname + `/../../test-states/${key}.txt`, { encoding: 'utf-8', flag: 'r+' });
+    return readFileSync(__dirname + `/../../test-states/${StateManager.makeKey(playwrightProjectName)}.txt`, {
+      encoding: 'utf-8',
+      flag: 'r+',
+    });
   }
 
   public static deleteProjectId(playwrightProjectName: string) {
-    const key = playwrightProjectName.split('-').slice(0, -1).join('-');
-    rmSync(__dirname + `/../../test-states/${key}.txt`);
+    rmSync(__dirname + `/../../test-states/${StateManager.makeKey(playwrightProjectName)}.txt`);
   }
 }
 

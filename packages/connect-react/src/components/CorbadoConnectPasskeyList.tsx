73952fa46b6b4f39234a097dedc54611c3aa32a3
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2024-07-10 15:18:39 +0200

    adds loading state

--- packages/connect-react/src/components/CorbadoConnectPasskeyList.tsx
+++ packages/connect-react/src/components/CorbadoConnectPasskeyList.tsx
@@ -9,7 +9,7 @@ import { CorbadoConnectPasskeyListConfig } from '@corbado/types';
 const CorbadoConnectPasskeyList: FC<CorbadoConnectPasskeyListConfig> = config => {
   return (
     <div className='light cb-width-full'>
-      <div className='cb-connect-container cb-width-full'>
+      <div className='cb-width-full cb-connect-container'>
         <ManageProcessProvider
           config={config}
           initialScreenType={ManageScreenType.Init}

4720ee51a824eaca22016bdb90d4ef07417faf6d
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2024-07-11 14:54:45 +0200

    makes bluetooth undefined if we can't access its info

--- packages/web-core/src/services/WebAuthnService.ts
+++ packages/web-core/src/services/WebAuthnService.ts
@@ -84,9 +84,17 @@ export class WebAuthnService {
     return window.PublicKeyCredential && (await window.PublicKeyCredential.isConditionalMediationAvailable());
   }
 
-  static async canUseBluetooth(): Promise<boolean> {
+  static async canUseBluetooth(): Promise<boolean | undefined> {
     try {
-      const availability = await navigator.bluetooth.getAvailability();
+      const bluetooth = navigator.bluetooth;
+
+      // When using Safari and Firefox navigator.bluetooth returns undefined => we will return undefined
+      if (!bluetooth) {
+        return;
+      }
+
+      const availability = await bluetooth.getAvailability();
+
       if (availability) {
         return true;
       }

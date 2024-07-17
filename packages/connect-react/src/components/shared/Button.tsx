b2b49e96dc897684b146953c8232e0118f7b04cf
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2024-07-17 15:16:07 +0200

    removes inline styles

--- packages/connect-react/src/components/shared/Button.tsx
+++ packages/connect-react/src/components/shared/Button.tsx
@@ -16,19 +16,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         {...rest}
       >
         {isLoading && (
-          <div
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: 'inherit',
-            }}
-          >
+          <div className='button-loading-container'>
             <LoadingSpinner variant='on-primary' />
           </div>
         )}

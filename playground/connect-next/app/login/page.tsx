e23b41fa182d5686e05ba13896d00ecddb06fe2a
Incorbador <incorbador@gmx.de>
2024-08-07 14:58:41 +0200

    Update vercel playground deployment

--- playground/connect-next/app/login/page.tsx
+++ playground/connect-next/app/login/page.tsx
@@ -2,7 +2,6 @@
 
 import { useRouter } from 'next/navigation';
 import { CorbadoConnectLogin } from '@corbado/connect-react';
-import Link from 'next/link';
 import { useState } from 'react';
 import ConventionalLogin from '@/app/login/ConventionalLogin';
 import { postPasskeyLogin } from '@/app/login/actions';

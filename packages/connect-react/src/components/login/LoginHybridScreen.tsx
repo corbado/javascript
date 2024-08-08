cf4d636a92bd45927dc767b4cf622bd16a0567f0
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2024-08-08 18:11:03 +0200

    adds login hybrid ui

---
+++ packages/connect-react/src/components/login/LoginHybridScreen.tsx
@@ -0,0 +1,50 @@
+import React, { useCallback } from 'react';
+
+import useLoginProcess from '../../hooks/useLoginProcess';
+import useShared from '../../hooks/useShared';
+import { LoginScreenType } from '../../types/screenTypes';
+import { LinkButton } from '../shared/LinkButton';
+import { PrimaryButton } from '../shared/PrimaryButton';
+import { HybridIcon } from '../shared/icons/HybridIcon';
+
+const LoginHybridScreen = () => {
+  const { config, navigateToScreen, currentIdentifier } = useLoginProcess();
+  const { getConnectService } = useShared();
+
+  const handleFallback = useCallback(() => {
+    navigateToScreen(LoginScreenType.Invisible);
+    config.onFallback(currentIdentifier);
+  }, [navigateToScreen, config, currentIdentifier]);
+
+  const handleExplicitFallback = useCallback(() => {
+    getConnectService().recordEventLoginExplicitAbort();
+    handleFallback();
+  }, [getConnectService, handleFallback]);
+
+  return (
+    <>
+      <div className='cb-h2'>Login with a mobile passkey</div>
+      <div className='cb-p'>A passkey was found on your mobile device.</div>
+      <div className='cb-login-hybrid-icons'>
+        <HybridIcon />
+      </div>
+      <div className='cb-p'>Use your mobile device to log in.</div>
+      <div className='cb-p'>You can use your mobile device to log in.</div>
+      <div className='cb-p'>Simply scan the QR code.</div>
+      <PrimaryButton
+        onClick={() => {}}
+        className='cb-login-hybrid-button'
+      >
+        Use mobile device
+      </PrimaryButton>
+      <LinkButton
+        onClick={handleExplicitFallback}
+        className='cb-login-hybrid-fallback'
+      >
+        Continue with email
+      </LinkButton>
+    </>
+  );
+};
+
+export default LoginHybridScreen;

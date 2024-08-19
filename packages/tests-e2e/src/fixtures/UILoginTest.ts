b704410bcb129364fca1627f89692c06bf1afc7a
Anders Choi <anders.choi@corbado.com>
2024-08-19 13:21:17 +0200

    print project id and process id per test

--- packages/tests-e2e/src/fixtures/UILoginTest.ts
+++ packages/tests-e2e/src/fixtures/UILoginTest.ts
@@ -4,9 +4,21 @@ import { UILoginFlow } from '../models/UILoginFlow';
 import StateManager from '../utils/StateManager';
 
 export const test = base.extend<{ loginFlow: UILoginFlow }>({
-  loginFlow: async ({ page }, use, testInfo) => {
+  loginFlow: async ({ page, context }, use, testInfo) => {
     const loginFlow = new UILoginFlow(page);
+
     loginFlow.projectId = StateManager.getProjectId(testInfo.project.name);
+    
+    const cboAuthProcessRaw = (await context.storageState()).origins
+      .find(origin => origin.origin.replace(/\/$/, '') === process.env.PLAYWRIGHT_TEST_URL?.replace(/\/$/, ''))
+      ?.localStorage.find(item => item.name === 'cbo_auth_process')?.value;
+    let processId = '_';
+    if (cboAuthProcessRaw) {
+      const cboAuthProcess = JSON.parse(cboAuthProcessRaw);
+      processId = cboAuthProcess.id;
+    }
+    console.log(testInfo.project.name, loginFlow.projectId, processId);
+
     await use(loginFlow);
   },
 });
--- packages/tests-e2e/src/fixtures/UISignupTest.ts
+++ packages/tests-e2e/src/fixtures/UISignupTest.ts
@@ -4,10 +4,20 @@ import { UISignupFlow } from '../models/UISignupFlow';
 import StateManager from '../utils/StateManager';
 
 export const test = base.extend<{ signupFlow: UISignupFlow }>({
-  signupFlow: async ({ page }, use, testInfo) => {
+  signupFlow: async ({ page, context }, use, testInfo) => {
     const signupFlow = new UISignupFlow(page);
 
     signupFlow.projectId = StateManager.getProjectId(testInfo.project.name);
+
+    const cboAuthProcessRaw = (await context.storageState()).origins
+      .find(origin => origin.origin.replace(/\/$/, '') === process.env.PLAYWRIGHT_TEST_URL?.replace(/\/$/, ''))
+      ?.localStorage.find(item => item.name === 'cbo_auth_process')?.value;
+    let processId = '_';
+    if (cboAuthProcessRaw) {
+      const cboAuthProcess = JSON.parse(cboAuthProcessRaw);
+      processId = cboAuthProcess.id;
+    }
+    console.log(testInfo.project.name, signupFlow.projectId, processId);
 
     await use(signupFlow);
   },

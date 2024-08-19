d9862c90e4725467d6d0984312f3e62be46ab035
Anders Choi <anders.choi@corbado.com>
2024-08-19 14:13:32 +0200

    increase timeout after page load

--- packages/tests-e2e/src/utils/constants.ts
+++ packages/tests-e2e/src/utils/constants.ts
@@ -54,4 +54,4 @@ export const emailLinkUrlToken = 'UaTwjBJwyDLMGVbR7WHh';
 
 export const operationTimeout = process.env.CI ? 3000 : 5000;
 export const totalTimeout = process.env.CI ? 20000 : 30000;
-export const waitAfterLoad = 600; // timeout to reduce flakiness due to repetitive reloads
+export const waitAfterLoad = 2000; // timeout to reduce flakiness due to repetitive reloads

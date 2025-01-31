8a90a3069613272f220b1c71f88f43c69b2523fe
Corbadoman <100508310+corbadoman@users.noreply.github.com>
2025-01-31 18:59:25 +0100

    Bugfixing

--- packages/web-core/src/models/auth.ts
+++ packages/web-core/src/models/auth.ts
@@ -1,12 +1,12 @@
 import type { AuthenticationRsp } from '../api/v1';
-import { ShortSession } from './session';
+import { SessionToken } from './sessionToken';
 
 export class AuthenticationResponse {
-  shortSession: ShortSession;
+  shortSession: SessionToken;
   redirectURL: string;
   longSession?: string;
 
-  constructor(shortSession: ShortSession, redirectURL: string, longSession?: string) {
+  constructor(shortSession: SessionToken, redirectURL: string, longSession?: string) {
     this.shortSession = shortSession;
     this.redirectURL = redirectURL;
     this.longSession = longSession;
@@ -17,6 +17,6 @@ export class AuthenticationResponse {
       throw new Error('ShortSession is undefined. This must never happen.');
     }
 
-    return new AuthenticationResponse(new ShortSession(value.shortSession.value), value.redirectURL, value.longSession);
+    return new AuthenticationResponse(new SessionToken(value.shortSession.value), value.redirectURL, value.longSession);
   }
 }

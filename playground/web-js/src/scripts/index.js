1d66515f2f0a72fcfccf1b6985f6308512e2cc5f
Corbadoman <100508310+corbadoman@users.noreply.github.com>
2025-02-07 07:14:12 +0100

    More backporting

--- playground/web-js/src/scripts/index.js
+++ playground/web-js/src/scripts/index.js
@@ -1,5 +1,5 @@
 import Corbado from '@corbado/web-js';
-import {CORBADO_PROJECT_ID, CORBADO_FRONTEND_API_URL_SUFFIX} from './environment';
+import { CORBADO_PROJECT_ID, CORBADO_FRONTEND_API_URL_SUFFIX } from './environment';
 
 let loaded = false;
 

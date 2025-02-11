eac66fd0ea3703358840aaa5adb39199974afaf2
Corbadoman <100508310+corbadoman@users.noreply.github.com>
2025-02-11 19:37:11 +0100

    Removed unsafe-eval

--- playground/connect-next/next.config.mjs
+++ playground/connect-next/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const cspHeader = `
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    script-src 'self' 'unsafe-inline';
 `;
 
 const nextConfig = {

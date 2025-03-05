92da058d6a86ed12fa8624ede4eb9684c785ddf6
aehnh <anders.choi@corbado.com>
2025-03-05 09:43:57 +0100

    increase CI timeout

--- packages/tests-e2e/src/connect/utils/Constants.ts
+++ packages/tests-e2e/src/connect/utils/Constants.ts
@@ -32,5 +32,5 @@ export enum WebhookTypes {
 export const phone = '+4915121609839';
 export const password = 'asdfasdf';
 
-export const totalTimeout = process.env.CI ? 30000 : 40000;
+export const totalTimeout = 40000;
 export const operationTimeout = 10000;

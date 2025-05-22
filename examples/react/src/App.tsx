b008b9654879bce7d793773652f5c27154f8c96d
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2025-05-22 15:21:38 +0200

    Update dependencies and implement telemetry event in React example

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,10 +1,23 @@
 import { CorbadoProvider } from '@corbado/react';
+import { sendEvent, TelemetryEventType } from '@corbado/shared-util';
 import RouteProvider from './routes';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { CustomizationsContext } from './contexts/CustomizationsContext';
 
 function App() {
   const { customTheme, darkMode, customTranslation } = useContext(CustomizationsContext);
+
+  useEffect(() => {
+    void sendEvent({
+      type: TelemetryEventType.EXAMPLE_APPLICATION_OPENED,
+      payload: {
+        exampleName: 'corbado/javascript/examples/react',
+      },
+      sdkVersion: '3.1.0',
+      sdkName: 'React SDK',
+      identifier: import.meta.env.VITE_CORBADO_PROJECT_ID,
+    });
+  }, []);
 
   return (
     <CorbadoProvider
